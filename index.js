76c0d3ae67dac42aeca44b22ba339fca0e10b2ec
kado.kado112 <108268013+kado-kado@users.noreply.github.com>
2025-05-03 14:47:13 +0900

    ver 2.0.3
    READMEが正しく含まれない問題を解決しました。

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const program = new Command();
 program
     .name("aes-url")
     .description("AES-encrypted URL disguiser CLI")
-    .version("2.0.2");
+    .version("2.0.3");
 
 const typeOptions = ["any", "today", "3day", "1week"];
 
